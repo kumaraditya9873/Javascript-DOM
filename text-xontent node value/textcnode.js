f8e99667966e4059e64eea4b7bcb744d0493a6e6
ADITYA <adityakumar9873@gmail.com>
2023-02-22 18:57:47 +0530

    textcnodes

---
+++ text-xontent node value/textcnode.js
@@ -0,0 +1,7 @@
+const text = document.getElementById("heading");
+// const value = text.nodeValue;
+// console.log(value);Wrong method
+
+// console.log(text.childNodes[0].nodeValue);
+// or
+console.log(text.firstChild.nodeValue);
