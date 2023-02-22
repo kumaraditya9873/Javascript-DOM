cf5bbcd59a0893a7a0e029eeee49204d4ca29b4e
ADITYA <adityakumar9873@gmail.com>
2023-02-22 22:56:28 +0530

    className classList updated

---
+++ classlist and class name/cls.js
@@ -0,0 +1,16 @@
+const first = document.getElementById("first");
+
+const value = first.className;
+
+console.log(value);
+
+const second = document.getElementById("second");
+const value1 = second.className;
+
+console.log(value1);
+
+// we can directly add the properties
+
+// ex
+
+second.className = "colors";
