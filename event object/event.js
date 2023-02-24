bd5c9718dec15037c28e498a8129f44259b221d5
ADITYA <adityakumar9873@gmail.com>
2023-02-24 22:46:47 +0530

    Event object

--- event object/event.js
+++ event object/event.js
@@ -1,6 +1,8 @@
 // dom events pass an Event object as an arguments to handelers
 // this objects contains extra information about the event that ocuured
-
+// event.type
+// event.currentTarget
+// this keyword
 // example = addEventListner("Click",function("any keyword that you want too see the object"){})
 
 const heading = document.querySelector("h1");
